77fccc67e6f5808274ad84571eef9f029f582b2e
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2022-01-30 13:03:18 -0600

    Find Closest Value In Bst

---
+++ src/algo/FindClosestValueInBst.java
@@ -0,0 +1,56 @@
+package algo;
+
+import static org.junit.Assert.assertEquals;
+
+public class FindClosestValueInBst {
+    public static int findClosestValueInBst(BST tree, int target) {
+        return findClosestValueInBst(tree, target, tree.value);
+    }
+
+    public static int findClosestValueInBst(BST tree, int target, int closest) {
+        BST currentNode = tree;
+        while(currentNode != null){
+            if(Math.abs(target - closest) > Math.abs(target - currentNode.value)){
+                closest = currentNode.value;
+            }if(target < currentNode.value){
+                currentNode = currentNode.left;
+            }else if(target > currentNode.value){
+                currentNode = currentNode.right;
+            }else{
+                break;
+            }
+        }
+        return closest;
+    }
+
+    static class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        var root = new BST(10);
+        root.left = new BST(5);
+        root.left.left = new BST(2);
+        root.left.left.left = new BST(1);
+        root.left.right = new BST(5);
+        root.right = new BST(15);
+        root.right.left = new BST(13);
+        root.right.left.right = new BST(14);
+        root.right.right = new BST(22);
+
+        var expected = 13;
+        var actual = FindClosestValueInBst.findClosestValueInBst(root, 12);
+       assertEquals(expected, actual);
+        if(expected == actual){
+            System.out.println(actual);
+        }else{
+            System.out.println(expected);
+        }
+    }
+}
