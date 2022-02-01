a62d0abd5e8bced6a2272f707e786be31636a05c
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2022-02-01 10:13:11 -0600

    Get Nth Fibonacci Second Solution

--- src/algo/GetNthFibonacci.java
+++ src/algo/GetNthFibonacci.java
@@ -1,17 +1,33 @@
 package algo;
 
+
 public class GetNthFibonacci {
     public static void main(String[] args) {
         System.out.println(getNthFib(4));
-
+        System.out.println(getNthFibSecondSolution(4));
     }
+
+    // O(log(n)) time | O(1) space
+    public static int getNthFibSecondSolution(int n) {
+        int[] lastTwo = {0, 1};
+        int counter = 3;
+        while (counter <= n) {
+            int nextFib = lastTwo[0] + lastTwo[1];
+            lastTwo[0] = lastTwo[1];
+            lastTwo[1] = nextFib;
+            counter++;
+        }
+        return n > 1 ? lastTwo[1] : lastTwo[0];
+    }
+
+    // O(log(2n)) time | O(n) space
     public static int getNthFib(int n) {
-        if(n == 2){
+        if (n == 2) {
             return 1;
-        }else if(n == 1){
+        } else if (n == 1) {
             return 0;
-        }else{
-            return getNthFib(n-1) + getNthFib(n-2);
+        } else {
+            return getNthFib(n - 1) + getNthFib(n - 2);
 
         }
     }
