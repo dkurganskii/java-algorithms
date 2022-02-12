2c4b152df0cf190d8fb1ab1177fea19fbe115fa2
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2022-02-12 09:31:52 -0600

    Palindrome check

---
+++ src/algo/PalindromeCheck.java
@@ -0,0 +1,22 @@
+package algo;
+
+// O(n) time, O(1) space
+public class PalindromeCheck {
+
+    public static boolean isPalindrome(String str){
+        int leftIdx = 0;
+        int rightIdx = str.length()-1;
+        while (leftIdx<rightIdx){
+            if (str.charAt(leftIdx) != str.charAt(rightIdx)){
+                return false;
+            }
+            leftIdx++;
+            rightIdx--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("madam"));
+    }
+}
